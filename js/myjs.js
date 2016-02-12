2bf7cbba6fcd7abf75124171e7ff487f8cc3391a
Alexa Schlegel <alexa.schlegel@gmail.com>
2016-02-12 19:02:56 +0100

    Added angular JS
    * added sup page routing
    * checking aout about navigation
    * home page when cklicking on brand (top left)
    * and last order list in menu

--- js/myjs.js
+++ js/myjs.js
@@ -1,26 +1,35 @@
-$(document).ready(function(){
-    
-    console.log("hello world");
+// myjs.js
 
-    $("#search-bar")
-        .animate({opacity: '0.5', height: '+=150px'},"slow")
-        .animate({opacity: '0.5', height: '-=150px'},"slow")
+// create the module and name it
+var barApp = angular.module('barApp', ['ngRoute']);
 
-    $("#test-btn").click(function(){
+// configure our routes
+barApp.config(function($routeProvider) {
+    $routeProvider
 
-        $.ajax({ 
-            type: 'GET', 
-            url: 'http://pub.jamaica-inn.net/fpdb/api.php', 
-            data: { username: 'ankov',
-                    password: 'ankov',
-                    action: 'purchases_get' }, 
-            dataType: 'json',
-            success: function (data) {
-                $.each(data.payload, function(index, element) {
-                    var $newelement = $( "<li class='list-group-item'>" + index + " : " + element.namn + "</li>");
-                    $('#test-api-result').append($newelement);
-                });
-            }
-        });
-    });
+        // route for the home page
+        .when('/', {
+            templateUrl : 'pages/home.html',
+            controller  : 'mainController'
+        })
+
+        // route for the last order page
+        .when('/last-orders', {
+            templateUrl : 'pages/last-orders.html',
+            controller  : 'lastOrdersController'
+        })
+});
+
+// create the controller and inject Angular's $scope
+barApp.controller('mainController', function($scope) {
+
+    // create a message to display in our view
+    $scope.message = 'This is the main page - like home.';
+});
+
+// create the controller and inject Angular's $scope
+barApp.controller('lastOrdersController', function($scope) {
+
+    // create a message to display in our view
+    $scope.message = 'Last oder page, will show last orders soon';
 });
