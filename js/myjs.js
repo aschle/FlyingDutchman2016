f580ac574e98bf9a803287230a42644eb132906c
Alexa Schlegel <alexa.schlegel@gmail.com>
2016-02-12 18:24:30 +0100

    Testing a little bit the API

--- js/myjs.js
+++ js/myjs.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
+    
     console.log("hello world");
 
     $("#search-bar")
         .animate({opacity: '0.5', height: '+=150px'},"slow")
         .animate({opacity: '0.5', height: '-=150px'},"slow")
+
+    $("#test-btn").click(function(){
+
+        $.ajax({ 
+            type: 'GET', 
+            url: 'http://pub.jamaica-inn.net/fpdb/api.php', 
+            data: { username: 'ankov',
+                    password: 'ankov',
+                    action: 'purchases_get' }, 
+            dataType: 'json',
+            success: function (data) {
+                $.each(data.payload, function(index, element) {
+                    var $newelement = $( "<li class='list-group-item'>" + index + " : " + element.namn + "</li>");
+                    $('#test-api-result').append($newelement);
+                });
+            }
+        });
+    });
 });
