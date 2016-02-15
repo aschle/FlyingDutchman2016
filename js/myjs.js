7a8c6229bbdbcad7d42e019d404db64eb9d72f22
Alexa Schlegel <alexa.schlegel@gmail.com>
2016-02-15 15:56:12 +0100

    some pages and some angular controllers

--- js/myjs.js
+++ js/myjs.js
@@ -18,6 +18,28 @@ barApp.config(function($routeProvider) {
             templateUrl : 'pages/last-orders.html',
             controller  : 'lastOrdersController'
         })
+
+        // route for alle beer page
+        .when('/all-beers', {
+            templateUrl : 'pages/all-beers.html',
+            controller  : 'allBeersController'
+        })
+});
+
+// Services are used for sharing information across controllers
+barApp.factory('UserService', function() {
+
+    factory = {};
+
+    factory.getCurrentUser = function() {
+        return "ankov";
+    }
+
+    factory.getPassword = function() {
+        return "ankov";
+    }
+
+    return factory;
 });
 
 // create the controller and inject Angular's $scope
@@ -27,9 +49,57 @@ barApp.controller('mainController', function($scope) {
     $scope.message = 'This is the main page - like home.';
 });
 
-// create the controller and inject Angular's $scope
-barApp.controller('lastOrdersController', function($scope) {
 
-    // create a message to display in our view
-    $scope.message = 'Last oder page, will show last orders soon';
+
+// create the controller and inject Angular's $scope
+barApp.controller('lastOrdersController', function($scope, $http) {
+
+    var username = "ankov";
+    var password = "ankov";
+    var action = "purchases_get";
+
+    $http({
+        method: 'GET',
+        url: 'http://pub.jamaica-inn.net/fpdb/api.php',
+        params: { username : username, password : password, action : action }
+    }).then(function successCallback(response) {
+
+        var sum = 0;
+        var count = 0;
+        $.each(response.data.payload, function(index, value){
+            sum += Number(value.price);
+            count += 1;
+        });
+
+        $scope.total = sum.toFixed(2);
+        $scope.items = count;
+        $scope.content = response.data.payload;
+    }, function errorCallback(response) {
+        $scope.content = "Something went wrong!";
+    });
 });
+
+barApp.controller('allBeersController', ['$scope', '$http', 'UserService',
+    function($scope, $http, UserService) { 
+        "use strict";
+
+        var action = "inventory_get";
+
+        $http({
+            method: 'GET',
+            url: 'http://pub.jamaica-inn.net/fpdb/api.php',
+            params: { username : "ankov", password : "ankov", action : action }
+        }).then(function successCallback(response) {
+
+            var count = 0;
+            $.each(response.data.payload, function(index, value){
+                count += 1;
+            });
+
+            $scope.items = count;
+            $scope.content = response.data.payload;
+        }, function errorCallback(response) {
+            $scope.content = "Something went wrong!";
+        });
+    }
+]);
