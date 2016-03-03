1ccf0145fa8695ffc23fd2ffbe7dd16f614cad5a
DaPandis <dafloatingpanda@gmail.com>
2016-03-03 18:03:02 +0100

    generated charts
    some bland statistics

--- js/controller/statistics-controller.js
+++ js/controller/statistics-controller.js
@@ -1,5 +1,5 @@
 (function () {
-    google.charts.load('current', {'packages':['corechart']});
+
     'use strict';
 
     angular
@@ -8,18 +8,18 @@
 
     StatisticsController.$inject = ['$scope', '$window', 'DataService', 'LocalStorageService'];
 
+
+
+    google.charts.load('current', {'packages':['corechart']});
     function StatisticsController($scope, $window, DataService, LSService) {
 
         $scope.init = function () {
 
 
-
             $('#menu-vip').show();
             $('#menu-admin').show();
-            $('.navbar .dropdown').show();
+            $('.navbar .container-fluid').show();
 
-            //google.charts.load('current', {'packages':['corechart']});
-            //google.charts.setOnLoadCallback(drawChart);
 
             var beers = [];
 
@@ -38,12 +38,13 @@
                         result = search(value.beer_id,beers);
                         value.name = value.namn;
                         if(result != -1) {
-
-                            beers[result].sold += 1;
+                            value.sold = 8;
+                            beers[result].push(value);
 
                         }else{
-                            value.sold = 1;
-                            beers.push(value);
+                            var newBeer = [];
+                            newBeer.push(value);
+                            beers.push(newBeer);
 
                         }
 
@@ -52,9 +53,8 @@
 
                 $scope.items = count;
                 $scope.content = beers;
+                $scope.length = beers.length;
 
-
-                google.charts.setOnLoadCallback(drawChart);
 
             }, function(response){
                 $scope.content = "Something went wrong!";
@@ -62,34 +62,10 @@
         };
 
 
-        //google.charts.load('current', {'packages':['corechart']});
-        //google.charts.setOnLoadCallback(drawChart);
-
-        function drawChart() {
-            var data = google.visualization.arrayToDataTable([
-                ['Year', 'Sales', 'Expenses'],
-                ['2004',  1000,      400],
-                ['2005',  1170,      460],
-                ['2006',  660,       1120],
-                ['2007',  1030,      540]
-            ]);
-
-            var options = {
-                title: 'Company Performance',
-                width:400,
-                height:300,
-
-            };
-
-            var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
-            chart.draw(data, options);
-        }
-
 
         function search(nameKey, myArray){
             for (var i=0; i < myArray.length; i++) {
-                if (myArray[i].beer_id == nameKey) {
+                if (myArray[i][0].beer_id == nameKey) {
                     return i;
                 }
 
@@ -97,6 +73,98 @@
             return -1;
         }
 
+        function searchDate(keyDate, myArray){
+            var count = 0;
+            for (var i=0; i < myArray.length; i++) {
+
+                if (myArray[i].date.getFullYear() == keyDate.getFullYear() &&
+                    myArray[i].date.getMonth() == keyDate.getMonth() &&
+                    myArray[i].date.getDate() == keyDate.getDate()) {
+                    count++;
+                }
+
+            }
+            return count;
+
+        }
+
+        function compareDate(a,b) {
+            if (a.date < b.date)
+                return -1;
+            else if (a.date > b.date)
+                return 1;
+            else
+                return 0;
+        }
+
+
+        function cleanData(values){
+            var newValues = [];
+            for (var i = 0; i < values.length; ++i) {
+                var dayTime =  values[i].timestamp.split(" ");
+
+                dayTime[0] =  dayTime[0].split("-");
+                dayTime[1] =  dayTime[1].split(":");
+
+                var aDate = new Date();
+                aDate.setFullYear(dayTime[0][0],dayTime[0][1]-1 ,dayTime[0][2]);
+                values[i].date = aDate;
+                values[i].time = dayTime[1][0] + dayTime[1][1] + dayTime[1][2];
+                //console.log(dayTime[0] + dayTime[1]);
+
+                newValues.push(values[i]);
+            }
+            return newValues.sort(compareDate);
+
+        }
+
+        $scope.addToGraph = function(values){
+
+
+
+            var cleanDates = cleanData(values);
+
+            var options = {
+                title: "Beverage Sales last 30 days",
+                width:500,
+                height:400,
+                legend: { position: 'none' },
+                chartArea: {  width: "85%", height: "70%" },
+                vAxis: {title:"sold", viewWindowMode:'explicit', format: '0',
+                    viewWindow:{
+                        max:50,
+                        min:0
+                    }},
+                hAxis:{title:"date"}
+
+            };
+
+            var dataArray =  [['saledate', values[0].name]];
+
+            var idate = new Date();
+            idate.setDate(idate.getDate() - 30);
+
+            var tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+
+
+            for (var i = idate; i < tomorrow; i.setDate(i.getDate() + 1)) {
+                var currentMonth = parseInt(i.getMonth());
+                currentMonth++;
+                var parse = searchDate(i,cleanDates);
+                if(parse != 0){
+                    dataArray.push([currentMonth.toString() +'/'+ i.getDate(), parse]);
+                }else{
+                    dataArray.push([currentMonth.toString() +'/'+ i.getDate(),0]);
+                }
+            }
+
+            var data = google.visualization.arrayToDataTable(dataArray);
+
+            var chart = new google.visualization.ColumnChart(document.getElementById('curve_chart'));
+            chart.draw(data, options);
+
+        }
 
 
 
@@ -104,4 +172,3 @@
 
     }
 })();
-
