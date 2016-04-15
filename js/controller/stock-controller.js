8b814dab307fbf33676428db54f2d4588ba3ae8d
Maxilicious <wijnbladh.max@gmail.com>
2016-04-15 16:36:50 +0200

    Documentation for stock-controller.js

--- js/controller/stock-controller.js
+++ js/controller/stock-controller.js
@@ -1,10 +1,12 @@
 /**
- * Created by max on 2016-02-26.
+ * File: stock-controller.js
+ * Author: Max Wijnbladh
+ *
+ * This controller is responsible for fetching the list of beverages in stock, as well as setting variables used in the stock
+ * page and defining the function that is to be run when the Update Stock button is clicked.
+ * 
  */
 
-/**
- * stock-controller.js
- */
 (function () {
     'use strict';
 
@@ -13,7 +15,7 @@
         .controller('StockController', StockController);
 
     StockController.$inject = ['$scope', '$window','LocalStorageService', 'DataService'];
-
+    //Init scope and start fetching the beverages in stock.
     function StockController($scope, $window, LSService, DataService) {
 
         $scope.init = function () {
@@ -33,23 +35,32 @@
                     }
 
                 });
-
+                
+                //The list of beverages
                 $scope.content = beers;
+                //The currently selected beer.
                 $scope.currentbeer = "";
+                //The ID for the currently selected beer
                 $scope.currentID = ""
+                //The price for the currently selected beer
                 $scope.currentPrice = "Price"
+                //The number of bottles in stock for the currently selected beer.
                 $scope.currentAmount = "Amount"
+                
 
-
+            //If  the fetching of beers fails, display error message
             }, function(response){
                 $scope.content = "Something went wrong!";
             });
 
 
         };
-
+        
+        //The function which updates the inventory. Takes the ID of the beer that is to be adjusted, the new price and the new number of bottles in stock as parameters.
         $scope.updateInventory = function (currentID, newPrice, newAmount) {
+            //Display the parameters in the console
             console.log(currentID, newPrice, newAmount);
+            //Run the function definied in the Dataservice.
             DataService.updateInventory(currentID, newAmount, newPrice);
 
         }
